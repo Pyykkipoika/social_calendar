ed2e156eb1c88516df3bc7b1b10bf3d0e3de36fb
Dan Garland <dan@dangarland.co.uk>
2013-12-13 18:31:16 +0000

    Adding travis friendly db setup

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131213120850) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "attendees", force: true do |t|
     t.integer  "event_id"
     t.integer  "participant_id"
